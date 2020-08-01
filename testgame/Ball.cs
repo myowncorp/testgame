800521dcc97bf75d675cc175b355b186993da838
ninyowacha <jacee215@yahoo.com>
2020-08-01 06:50:08 -0400

    i made the ballclass even better

---
+++ testgame/Ball.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace testgame.Desktop
+{
+
+    public class Ball
+    {
+        public Texture2D Skin;
+        public Vector2   Pos;  
+        public float     X;   
+        public float     Y;   
+        public float     Spd;  
+        public int       Ax;   
+        public int       Ay;
+
+        public Ball(Texture2D ballTexture,
+                    Vector2   ballPosition,
+                    float     ballSpeed,
+                    int       ballXAccel,
+                    int       ballYAccel){
+            
+            Skin = ballTexture;
+            Pos  = ballPosition;
+            X    = ballPosition.X;
+            Y    = ballPosition.Y;
+            Spd  = ballSpeed;
+            Ax   = ballXAccel;
+            Ay   = ballYAccel;
+        }
+    }
+}
