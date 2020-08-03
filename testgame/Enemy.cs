ba583daa55342ee28f489f66a4671352f6c24115
ninyowacha <jacee215@yahoo.com>
2020-08-03 01:08:47 -0400

    very basic outline of EnemyClass

---
+++ testgame/Enemy.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+namespace testgame.Desktop
+
+{
+    public class Enemy
+    {
+        
+        public Texture2D Skin;  
+        public Vector2   Pos;
+        public float     Spd; 
+        public int       Ax;  
+        public int       Ay;
+        public int       Dmg;
+
+        public Enemy(Texture2D enemyTexture,
+                     Vector2   enemyPos,
+                     float     enemySpeed,
+                     int       enemyXAccel,
+                     int       enemyYAccel
+                     int       enemyDamage){
+
+
+            Skin = enemyTexture;
+            Pos  = enemyPos;
+            Spd  = enemySpeed;
+            Ax   = enemyXAccel;
+            Ay   = enemyYAccel;
+            Dmg  = enemyDamage;
+
+            
+        }
+    }
+}
