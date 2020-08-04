f1811fa18d881fe3a0778f320643e9f137b7c8a6
Edgar Carballo <karvayoEdgar@gmail.com>
2020-08-03 20:55:43 -0700

    Enemies should follow a point sequence

--- testgame/Enemy.cs
+++ testgame/Enemy.cs
@@ -1,43 +1,33 @@
 ﻿using System;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using System.Collections.Generic;
 
 namespace testgame.Desktop
-
 {
-    public class Enemy
-    {
-        
-        public Texture2D      Skin;  
-        public Vector2        Pos;
-        public float          Spd; 
-        public int            Ax;
-        public int            Ay;
-        public int            Dmg;
-        public List<Vector2>  Path;
+	public class Enemy
+	{
+		public Enemy(Texture2D enemyTexture,
+								 float enemySpeed,
+								 int enemyDamage,
+								 Path path)
+		{
+			Skin = enemyTexture;
+			Speed = enemySpeed;
+			Dmg = enemyDamage;
+			Path = path ?? throw new ArgumentNullException();
+		}
 
+		public int Dmg { get; set; }
+		public Path Path { get; }
+		public Vector2 Pos => Path.GetLocationAtTime(pathPosition);
+		public Texture2D Skin { get; set; }
+		public float Speed { get; set; }
 
+		public void UpdPos(GameTime time)
+		{
+			if (Path != null) pathPosition += (float)time.ElapsedGameTime.TotalSeconds * Speed;
+		}
 
-        public Enemy(Texture2D     enemyTexture,
-                     Vector2       enemyPos,
-                     float         enemySpeed,
-                     int           enemyXAccel,
-                     int           enemyYAccel,
-                     int           enemyDamage,
-                     List<Vector2> enemyPath
-                     ){
-
-
-            Skin = enemyTexture;
-            Pos  = enemyPos;
-            Spd  = enemySpeed;
-            Ax   = enemyXAccel;
-            Ay   = enemyYAccel;
-            Dmg  = enemyDamage;
-            Path = enemyPath;
-        }
-
-        public void UpdPos(){ }
-    }
+		private float pathPosition;
+	}
 }
---
+++ testgame/Path.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+public class Path
+{
+	public Path(params Vector2[] vertices)
+	{
+		this.vertices = vertices;
+	}
+
+	public Vector2 End => vertices[vertices.Length - 1];
+	public int SectionCount => vertices.Length - 1;
+	public Vector2 Start => vertices[0];
+
+	public Vector2 GetLocationAtTime(float time)
+	{
+		if (time <= 0) return Start;
+		else if (time >= SectionCount) return End;
+
+		var section = (int)Math.Floor(time);
+		var interPolationTime = time - section;
+
+		var start = vertices[section];
+		var end = vertices[section + 1];
+		return (1 - interPolationTime) * start + (interPolationTime) * end;
+	}
+
+	private readonly Vector2[] vertices;
+}
